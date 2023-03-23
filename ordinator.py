d97afb800ca6996aeb658517ba5ad6ac06fdc630
Seaver Olson <46256937+seaver-olson@users.noreply.github.com>
2023-03-23 14:24:54 +0000

    changed ordinator from function-based to object-oriented

--- ordinator.py
+++ ordinator.py
@@ -1,41 +1,43 @@
 import serial, time, tkinter as tk, PIL.ImageGrab as ImageGrab
-#get screen size
+
+
 root = tk.Tk()
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
-#make sure every value in tuple is int
-top_left = tuple(map(int, (0, 0, 0.5 * screen_width, 0.5 * screen_height)))
-top_right = tuple(map(int, (0.5 * screen_width, 0, screen_width, 0.5 * screen_height)))
-bottom_left = tuple(map(int, (0, 0.5 * screen_height, 0.5 * screen_width, screen_height)))
-bottom_right = tuple(map(int, (0.5 * screen_width, 0.5 * screen_height, screen_width, screen_height)))
-middle_left = tuple(map(int, (0, 0.25 * screen_height, 0.5 * screen_width, 0.75 * screen_height)))
-middle_right = tuple(map(int, (0.5 * screen_width, 0.25 * screen_height, screen_width, 0.75 * screen_height)))
-top_middle = tuple(map(int, (0.25 * screen_width, 0, 0.75 * screen_width, 0.5 * screen_height)))
-bottom_middle = tuple(map(int, (0.25 * screen_width, 0.5 * screen_height, 0.75 * screen_width, screen_height)))
-#rename the variables like your a pirate
-
-# Define a function to calculate the average pixel color in a given section of the screen
-def get_average_color(box):
-
-    #take image of screen first and then crop
-    pixels = ((ImageGrab.grab()).crop(box)).getdata()      
-    num_pixels = len(pixels) 
-    r = g = b = 0                  
-    for pixel in pixels:
-        r += pixel[0]             
-        g += pixel[1]     
-        b += pixel[2]   
-    avg_r, avg_g, avg_b = [x // num_pixels for x in (r, g, b)]
-
-    return str('%02x%02x%02x' % (avg_r, avg_g, avg_b))
+root.destroy()
+class Ordinator:
+    """get the average color of 8 boxes on the screen so that I can do less math on arduino"""
+    def __init__(self,screen_width,screen_height):
+        self.screenW = screen_width
+        self.screenH = screen_height
+        self.top_left = tuple(map(int, (0, 0, 0.5 * self.screenW, 0.5 * self.screenH)))
+        self.top_right = tuple(map(int, (0.5 * self.screenW, 0, self.screenW, 0.5 * self.screenH)))
+        self.bottom_left = tuple(map(int, (0, 0.5 * self.screenH, 0.5 * self.screenW, self.screenH)))
+        self.bottom_right = tuple(map(int, (0.5 * self.screenW, 0.5 * self.screenH, self.screenW, self.screenH)))
+        self.middle_left = tuple(map(int, (0, 0.25 * self.screenH, 0.5 * self.screenW, 0.75 * self.screenH)))
+        self.middle_right = tuple(map(int, (0.5 * self.screenW, 0.25 * self.screenH, self.screenW, 0.75 * self.screenH)))
+        self.top_middle = tuple(map(int, (0.25 * self.screenW, 0, 0.75 * self.screenW, 0.5 * self.screenH)))
+        self.bottom_middle = tuple(map(int, (0.25 * self.screenW, 0.5 * self.screenH, 0.75 * self.screenW, self.screenH)))
+    #turn get_average_color into a class method
+    def get_average_color(self,box):
+        """Uses PIL to get the average color of a box on the screen"""
+        pixels = ((ImageGrab.grab()).crop(box)).getdata()
+        num_pixels = len(pixels)
+        r = g = b = 0
+        for pixel in pixels:
+            r += pixel[0]
+            g += pixel[1]
+            b += pixel[2]
+        avg_r, avg_g, avg_b = [x // num_pixels for x in (r, g, b)]
+        return str('%02x%02x%02x' % (avg_r, avg_g, avg_b))
+    #turn get_all_colors into a class method
+    def get_all_colors(self):
+        """Returns a string of all the colors in the order of the boxes"""
+        return (self.get_average_color(self.top_left) + "|" + self.get_average_color(self.top_right) + "|" + self.get_average_color(self.bottom_left) + "|" + self.get_average_color(self.bottom_right) + "|" + self.get_average_color(self.middle_left) + "|" + self.get_average_color(self.middle_right) + "|" + self.get_average_color(self.top_middle) + "|" + self.get_average_color(self.bottom_middle))
     
-
-def get_all_colors():return (get_average_color(top_left) + "|" + get_average_color(top_right) + "|" + get_average_color(bottom_left) + "|" + get_average_color(bottom_right) + "|" + get_average_color(middle_left) + "|" + get_average_color(middle_right) + "|" + get_average_color(top_middle) + "|" + get_average_color(bottom_middle))
-#ex output of get_all_colors() = "ffffff|ffffff|ffffff|ffffff|ffffff|ffffff|ffffff|ffffff"
+Ord = Ordinator(screen_width,screen_height)
 while True:
-    #if serial port is available, send the data
-    if serial.Serial('/dev/tty.usbmodem14201', 9600):
-        ser = serial.Serial('/dev/tty.usbmodem14201', 9600)
-        ser.write(get_all_colors().encode())
-        ser.close()
-    time.sleep(0.1)
+    ser = serial.Serial('/dev/tty.usbmodem14201', 9600)
+    ser.write(Ord.get_all_colors().encode())
+    ser.close()
+    time.sleep(5)
