4ef45344d51a6fd9f8bcb0b198daa0c26c670e13
Seaver Olson <46256937+seaver-olson@users.noreply.github.com>
2023-03-24 14:49:46 +0000

    add random colors

--- main.ino
+++ main.ino
@@ -8,18 +8,20 @@ Date: 3/15/2023
 #define DATA_PIN 8
 
 String top_left, top_right, bottom_left, bottom_right, middle_left, middle_right, top_middle, bottom_middle;
-
 const int part_num = NUM_LEDS / 8;
-//create objects that represent the parts of the LED strip
-//top left
+
 CRGB leds[NUM_LEDS];//create object
 void setup() { 
     //specs: 3 pin 5v RGB LED strip
     Serial.begin(9600);
     FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);//defines ledtype and pin
-    //divide strip into 8 parts
 }
+typedef void (*SimplePatternList[])();
+SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
 
+uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+  
 String getScreenColor(){
     //ordinator.py will return a string of the color of the screen
     //listen for the string once a line is recieved stop listening and take the most recent line
@@ -32,18 +34,57 @@ String getScreenColor(){
       return "";
     }
 }
-void patternRandom(){
-    //when the user doesn't want adaptive lighting choose from random other designs
+void rainbow() {
+    // FastLED's built-in rainbow generator
+    fill_rainbow( leds, NUM_LEDS, gHue, 7);
+}
+void rainbowWithGlitter() {
+    // built-in FastLED rainbow, plus some random sparkly glitter
+    rainbow();
+    addGlitter(80);
+}
+void addGlitter( fract8 chanceOfGlitter) {
+    if( random8() < chanceOfGlitter) {
+        leds[ random16(NUM_LEDS) ] += CRGB::White;
+    }
+}
+void confetti() {
+    // random colored speckles that blink in and fade smoothly
+    fadeToBlackBy( leds, NUM_LEDS, 10);
+    int pos = random16(NUM_LEDS);
+    leds[pos] += CHSV( gHue + random8(64), 200, 255);
+}
+void sinelon() {
+    // a colored dot sweeping back and forth, with fading trails
+    fadeToBlackBy( leds, NUM_LEDS, 20);
+    int pos = beatsin16( 13, 0, NUM_LEDS-1 );
+    leds[pos] += CHSV( gHue, 255, 192);
+}
+void bpm() {
+    // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
+    uint8_t BeatsPerMinute = 62;
+    CRGBPalette16 palette = PartyColors_p;
+    uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+    for( int i = 0; i < NUM_LEDS; i++) { //9948
+        leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
+    }
+}
+void juggle() {
+    // eight colored dots, weaving in and out of sync with each other
+    fadeToBlackBy( leds, NUM_LEDS, 20);
+    byte dothue = 0;
+    for( int i = 0; i < 8; i++) {
+        leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(dothue, 200, 255);
+        dothue += 32;
+    }
+}
+void changePattern(){
+    //change pattern
+    gCurrentPatternNumber = (gCurrentPatternNumber + 1) % 6;
+    gHue = 0;
+}
 
-}
-void patternRainbow(){
-    //FastLED rainbow pattern
-    for (int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = CHSV(i, 255, 255);
-        }
-    FastLED.show();
-    delay(1000 / 60);//60 fps
-}
+
 void patternSolid(){
     //solid color
     for (int i = 0; i < NUM_LEDS; i++) {
