0904f7b159579b0fae871c15114cc984b1b3b650
Seaver Olson <46256937+seaver-olson@users.noreply.github.com>
2023-03-31 09:30:56 -0500

    Create dependencies.sh

---
+++ dependencies.sh
@@ -0,0 +1,5 @@
+python3 -m pip install pyserial
+python3 -m pip install tk
+python3 -m pip install Pillow
+echo("all python libraries installed")
+echo("please install the lastest version of FastLED through Arduino package manager")
