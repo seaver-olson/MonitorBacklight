334afb958b69033526340026087b30c0ea8edeb5
Seaver Olson <46256937+seaver-olson@users.noreply.github.com>
2023-05-05 10:18:26 -0500

    Update basic.py

--- code/basic.py
+++ code/basic.py
@@ -1,4 +1,4 @@
-import serial, time, tkinter as tk, PIL.ImageGrab as ImageGrab,numpy as np
+import serial, time, tkinter as tk, PIL.ImageGrab as ImageGrab,numpy as np,csv
 
 
 root = tk.Tk()
@@ -9,46 +9,37 @@ class Ordinator:
     """get the average color of 8 boxes on the screen so that I can do less math on arduino"""
     def __init__(self,sw,sh):
         self.screenW = sw
+        self.n = 10
         self.screenH = sh
-        self.top_left = tuple(map(int, (0, 0, 0.5 * self.screenW, 0.5 * self.screenH)))
-        self.top_right = tuple(map(int, (0.5 * self.screenW, 0, self.screenW, 0.5 * self.screenH)))
-        self.bottom_left = tuple(map(int, (0, 0.5 * self.screenH, 0.5 * self.screenW, self.screenH)))
-        self.bottom_right = tuple(map(int, (0.5 * self.screenW, 0.5 * self.screenH, self.screenW, self.screenH)))
-        self.middle_left = tuple(map(int, (0, 0.25 * self.screenH, 0.5 * self.screenW, 0.75 * self.screenH)))
-        self.middle_right = tuple(map(int, (0.5 * self.screenW, 0.25 * self.screenH, self.screenW, 0.75 * self.screenH)))
-        self.top_middle = tuple(map(int, (0.25 * self.screenW, 0, 0.75 * self.screenW, 0.5 * self.screenH)))
-        self.bottom_middle = tuple(map(int, (0.25 * self.screenW, 0.5 * self.screenH, 0.75 * self.screenW, self.screenH)))
+        self.top_left = tuple(map(int, (0, 0, 0.33 * self.screenW, 0.33 * self.screenH)))
+        self.top_right = tuple(map(int, (0.66 * self.screenW, 0, self.screenW, 0.33 * self.screenH)))
+        self.top_middle = tuple(map(int, (0.33 * self.screenW, 0, 0.66 * self.screenW, 0.33 * self.screenH)))
+        self.middle_left = tuple(map(int, (0, 0.33 * self.screenH, 0.33 * self.screenW, 0.66 * self.screenH)))
+        self.middle_right = tuple(map(int, (0.66 * self.screenW, 0.33 * self.screenH, self.screenW, 0.66 * self.screenH)))
+        self.bottom_left = tuple(map(int, (0, 0.66 * self.screenH, 0.33 * self.screenW, self.screenH)))
+        self.bottom_middle = tuple(map(int, (0.33 * self.screenW, 0.66 * self.screenH, 0.66 * self.screenW, self.screenH)))
+        self.bottom_right = tuple(map(int, (0.66 * self.screenW, 0.66 * self.screenH, self.screenW, self.screenH)))
         self.boxes = [self.top_right,self.top_middle,self.top_left,self.middle_left,self.bottom_left,self.bottom_middle,self.bottom_right,self.middle_right]
     def get_average_color(self,box,pixels):
+        print(box)
         """Uses PIL to get the average color of a box on the screen"""
-        #pixels = [pixels[x,y] for x in range(box[0],box[2],2) for y in range(box[1],box[3],2)]
-        n = 2
+        pixels = [pixels[x,y] for x in range(box[0],box[2],self.n) for y in range(box[1],box[3],self.n)]
         pixels = np.array(pixels)
-        subarray = pixels[box[0]:box[2]:n, box[1]:box[3]:n]#get every nth pixel
-
-        # flatten the subarray into a 1D array
-        pixels = subarray.flatten().tolist()
         avg_r,avg_g,avg_b  =map(lambda x: x.zfill(3),[str(np.mean(pixels[:,0],dtype=np.int64)),str(np.mean(pixels[:,1],dtype=np.int64)),str(np.mean(pixels[:,2],dtype=np.int64))])
         return avg_r+ avg_g +avg_b
-    '''
-    def get_average_color(self, box, pixels):
-    """Uses PIL to get the average color of a box on the screen"""
-        pixels = np.array(pixels, dtype=np.uint8)
-        subarray = pixels[box[0]:box[2]:2, box[1]:box[3]:2]
-        avg_r, avg_g, avg_b = map(lambda x: str(int(x)).zfill(3), map(np.mean, [subarray[:,:,i] for i in range(3)]))
-        return avg_r + avg_g + avg_b
-    
-    '''
     #turn get_all_colors into a class method
-    def get_all_colors(self):
+    def get_all_colors(self,n: int):
+        self.n = n
         """Returns a string of all the colors in the order of the boxes"""
         self.pixels = (ImageGrab.grab().load())
-        return ''.join(map(self.get_average_color,self.boxes,[self.pixels]*8))
-
+        return ''.join(map(self.get_average_color,self.boxes,[self.pixels]*8)).rstrip()
 Ord = Ordinator(screen_width,screen_height)
-
+ser = serial.Serial(port='/dev/tty.usbmodem14201', baudrate=9600)
+time.sleep(2)
 while True:
-    ser = serial.Serial('/dev/tty.usbmodem14201', 230400)
-    ser.write(Ord.get_all_colors().encode())
-    print(Ord.get_all_colors())
-    ser.close()
+    x = (Ord.get_all_colors(10))
+    ser.write(bytes(x, 'utf-8'))
+    print(x)
+    time.sleep(.8)
+ser.close()
+
